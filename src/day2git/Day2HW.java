70df9a7ab4444f4c03274b763797eeead982f4b4
GYTumlisan <gymoa2011@gmail.com>
2023-04-08 10:05:42 -0700

    Initial commit day2HW

---
+++ src/day2git/Day2HW.java
@@ -0,0 +1,12 @@
+package day2git;
+
+public class Day2HW {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Local repository");
+		
+		System.out.println("Remote repository");
+	}
+
+}
